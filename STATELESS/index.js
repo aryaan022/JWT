d319b7d893a8dc48737a1ee862ecd978731ece64
Aryan Khokhar <aryankhokhar022@gmail.com>
2025-09-07 20:12:06 +0530

    Used bcrypt

--- STATELESS/index.js
+++ STATELESS/index.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 
+// MongoDB connection
+
+mongoose.connect('mongodb://localhost:27017/jw_token')
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+
+
+ 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
